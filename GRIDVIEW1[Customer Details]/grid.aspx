06fe26498a79565c959bad2bda83b28718a821fc
Boopathi Subramanian <109845477+boopathiskv@users.noreply.github.com>
2023-05-25 22:52:39 +0530

    frist commit
    this full code

---
+++ GRIDVIEW1[Customer Details]/grid.aspx
@@ -0,0 +1,93 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="grid.aspx.cs" Inherits="GRIDVIEW1_Customer_Details_.grid" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            width: 41%;
+        }
+        .auto-style2 {
+            width: 140px;
+        }
+        .auto-style3 {
+            width: 322px;
+        }
+        .auto-style4 {
+            width: 140px;
+            text-align: left;
+        }
+        .auto-style5 {
+            width: 322px;
+            text-align: left;
+        }
+        .auto-style6 {
+            margin-top: 0px;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div align="center">
+            <u><h2>Customer Details</h2></u>
+        </div>
+        <div align="center">
+
+            <table class="auto-style1">
+                <tr>
+                    <td class="auto-style2">&nbsp;</td>
+                    <td class="auto-style3">&nbsp;</td>
+                    <td>&nbsp;</td>
+                </tr>
+                <tr>
+                    <td class="auto-style4">Eid</td>
+                    <td class="auto-style5">
+                        <asp:TextBox ID="txtid" runat="server"  Width="220px"></asp:TextBox>
+                    </td>
+                    <td>&nbsp;</td>
+                </tr>
+                <tr>
+                    <td class="auto-style4">EName</td>
+                    <td class="auto-style5">
+                        <asp:TextBox ID="txtna" runat="server" Width="220px"></asp:TextBox>
+                    </td>
+                    <td>&nbsp;</td>
+                </tr>
+                <tr>
+                    <td class="auto-style4">Esalary</td>
+                    <td class="auto-style5">
+                        <asp:TextBox ID="txtsal" runat="server" Width="220px"></asp:TextBox>
+                    </td>
+                    <td>&nbsp;</td>
+                </tr>
+                <tr>
+                    <td class="auto-style2">&nbsp;</td>
+                    <td class="auto-style5">
+                        <br />
+                        <asp:Button ID="btnsav" runat="server" Text="SAVE" CssClass="auto-style6" Height="32px" OnClick="btnsav_Click" Width="79px" />  &nbsp&nbsp&nbsp&nbsp&nbsp
+                        <asp:Button ID="btncan" runat="server" Text="CANCEL" Height="33px" OnClick="btncan_Click" Width="75px" />
+                    </td>
+                    <td>&nbsp;</td>
+                </tr>
+            </table>
+
+        </div>
+        <br/>
+        <div align="center">
+
+            <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="False" DataKeyNames="eid" OnRowEditing="GridView1_RowEditing" OnRowUpdating="GridView1_RowUpdating" Width="374px" OnRowCancelingEdit="GridView1_RowCancelingEdit" OnRowDeleting="GridView1_RowDeleting">
+                <Columns>
+                    <asp:BoundField DataField="eid" HeaderText="EID" ReadOnly="True" />
+                    <asp:BoundField DataField="ename" HeaderText="EName" />
+                    <asp:BoundField DataField="esalary" HeaderText="ESalary" />
+                    <asp:CommandField ButtonType="Button" ShowEditButton="True" />
+                    <asp:CommandField ButtonType="Button" ShowDeleteButton="True" />
+                </Columns>
+            </asp:GridView>
+
+        </div>
+    </form>
+</body>
+</html>
---
+++ GRIDVIEW1[Customer Details]/grid.aspx.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace GRIDVIEW1_Customer_Details_
+{
+    public partial class grid : System.Web.UI.Page
+    {
+        string cs = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if(!IsPostBack)
+            {
+                txtid.Focus();
+                Load();
+            }
+        }
+
+       protected void Load()
+        {
+            using (SqlConnection con = new SqlConnection(cs))
+            {
+                string sql = "select * from Employ";
+                con.Open();
+                SqlCommand cmd = new SqlCommand(sql, con);
+                SqlDataReader dr = cmd.ExecuteReader();
+                if (dr.HasRows == true)
+                {
+                    GridView1.DataSource = dr;
+                    GridView1.DataBind();
+                }
+                con.Close();
+            }
+
+        }
+        void Clear()
+        {
+            txtid.Text = "";
+            txtna.Text = "";
+            txtsal.Text = "";
+        }
+
+        protected void btnsav_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection con = new SqlConnection(cs))
+            {
+                string sql = "insert into Employ values('"+txtid.Text+ "','" + txtna.Text + "','" + txtsal.Text + "')";
+                con.Open();
+                SqlCommand cmd = new SqlCommand(sql, con);
+                int t = cmd.ExecuteNonQuery();
+                if(t>0)
+                {
+                    Response.Write("<script>alert('Inserted Successfull')</script>");
+                }
+                con.Close();
+                Load();
+                Clear();
+            }
+        }
+
+        protected void btncan_Click(object sender, EventArgs e)
+        {
+            Clear();
+        }
+
+        protected void GridView1_RowEditing(object sender, GridViewEditEventArgs e)
+        {
+            GridView1.EditIndex = e.NewEditIndex;
+            Load();
+        }
+
+        protected void GridView1_RowUpdating(object sender, GridViewUpdateEventArgs e)
+        {
+            int id = Convert.ToInt32(GridView1.DataKeys[e.RowIndex].Value.ToString());
+            string name = ((TextBox)GridView1.Rows[e.RowIndex].Cells[1].Controls[0]).Text;
+            string salary = ((TextBox)GridView1.Rows[e.RowIndex].Cells[2].Controls[0]).Text;
+
+            using (SqlConnection con = new SqlConnection(cs))
+            {
+                string sql = "update Employ set ename='" + name + "',esalary='" + salary + "' where eid='" +id+"'  ";
+                SqlCommand cmd = new SqlCommand(sql, con);
+                con.Open();
+                int t = cmd.ExecuteNonQuery();
+                if(t>0)
+                {
+                    Response.Write("<script>alert('updated')</script>");
+                    GridView1.EditIndex = -1;
+                    Load();
+
+                }
+                con.Close();
+            }
+         }
+
+        protected void GridView1_RowCancelingEdit(object sender, GridViewCancelEditEventArgs e)
+        {
+            GridView1.EditIndex = -1;
+            Load();
+        }
+
+        protected void GridView1_RowDeleting(object sender, GridViewDeleteEventArgs e)
+        {
+            int id = Convert.ToInt32(GridView1.DataKeys[e.RowIndex].Value.ToString());
+
+
+            using (SqlConnection con = new SqlConnection(cs))
+            {
+                string sql ="delete Employ where eid='"+id+"'";
+                SqlCommand cmd = new SqlCommand(sql, con);
+                con.Open();
+                int res = cmd.ExecuteNonQuery();
+                if(res==1)
+                {
+                    Response.Write("<script>alert('Deleted')</script>");
+
+                    GridView1.EditIndex = -1;
+                    Load();
+                }
+            }
+
+        }
+    }
+}
